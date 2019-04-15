ef5fd5100283816eb1ac592da2ca6c17a952e21d
Bart Heupers <bart.heupers@gmail.com>
2019-04-15 11:17:43 +0200

    Set data directory to location that exists in docker image

--- src/stadsarchief/settings.py
+++ src/stadsarchief/settings.py
@@ -64,8 +64,9 @@ OBJECTSTORE = dict(
 )
 
 PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
-DATA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'data'))
-
+DATA_DIR = '/app/data'
+if not os.path.exists(DATA_DIR):
+    DIVA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'data'))
 
 # SWAGGER
 SWAG_PATH = 'acc.api.data.amsterdam.nl/stadsarchief/docs'
