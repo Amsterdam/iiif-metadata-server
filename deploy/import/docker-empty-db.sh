85a2d9958cef19d13d51ef581fc514dbdca7606d
Yahia Elsherbini <yahia.elsherbini@elements.nl>
2020-04-10 12:45:13 +0200

    Removed wait-for-it from non docker scripts

--- deploy/import/docker-empty-db.sh
+++ deploy/import/docker-empty-db.sh
@@ -5,6 +5,4 @@ set -u   # crash on missing environment variables
 set -e   # stop on any error
 set -x   # log every command.
 
-source /deploy/wait-for-it.sh database:5432
-
 python manage.py run_import --delete --skip_validate_import --skipgetfiles --skipimport --skip_add_bag_ids
--- deploy/import/docker-import-db.sh
+++ deploy/import/docker-import-db.sh
@@ -4,8 +4,6 @@ set -u   # crash on missing environment variables
 set -e   # stop on any error
 set -x   # log every command.
 
-source /deploy/wait-for-it.sh database:5432
-
 # load data in database
 python manage.py migrate
 # Do full import. Delete all data
