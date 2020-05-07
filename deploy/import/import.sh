c10bad77a7e5322dd70462858295c10a34416c78
Yahia Elsherbini <yahia.elsherbini@elements.nl>
2020-05-07 15:09:13 +0200

    Changed db name to iiif_metadata_server because hyphens aren't allowed

--- deploy/import/import.sh
+++ deploy/import/import.sh
@@ -28,13 +28,13 @@ dc run importer /deploy/docker-migrate.sh
 echo "Load latest verblijfsobjecten, ligplaatsen, standplaatsen, nummeraanduidingen en panden in iiif-metadata-server database"
 
 # dc exec -T database update-db.sh atlas
-dc exec -T database update-table.sh bag bag_verblijfsobject public iiif-metadata-server
-dc exec -T database update-table.sh bag bag_ligplaats public iiif-metadata-server
-dc exec -T database update-table.sh bag bag_standplaats public iiif-metadata-server
-dc exec -T database update-table.sh bag bag_nummeraanduiding public iiif-metadata-server
-dc exec -T database update-table.sh bag bag_pand public iiif-metadata-server
-dc exec -T database update-table.sh bag bag_verblijfsobjectpandrelatie public iiif-metadata-server
-dc exec -T database update-table.sh bag bag_openbareruimte public iiif-metadata-server
+dc exec -T database update-table.sh bag bag_verblijfsobject public iiif_metadata_server
+dc exec -T database update-table.sh bag bag_ligplaats public iiif_metadata_server
+dc exec -T database update-table.sh bag bag_standplaats public iiif_metadata_server
+dc exec -T database update-table.sh bag bag_nummeraanduiding public iiif_metadata_server
+dc exec -T database update-table.sh bag bag_pand public iiif_metadata_server
+dc exec -T database update-table.sh bag bag_verblijfsobjectpandrelatie public iiif_metadata_server
+dc exec -T database update-table.sh bag bag_openbareruimte public iiif_metadata_server
 
 
 echo "Importing data"
@@ -44,6 +44,6 @@ echo "Drop bag tables used for importing"
 docker-compose -p iiif-metadata-server -f ${DIR}/docker-compose.yml exec -T database psql -U stadsarchief -c 'DROP TABLE bag_openbareruimte, bag_verblijfsobjectpandrelatie, bag_pand, bag_nummeraanduiding, bag_standplaats, bag_ligplaats, bag_verblijfsobject' stadsarchief
 
 echo "Running backups"
-dc exec -T database backup-db.sh iiif-metadata-server
+dc exec -T database backup-db.sh iiif_metadata_server
 dc down -v
 echo "Done"
