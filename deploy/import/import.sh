e35c94f816eee8a9589b0ebe74c208905402bd6e
Yahia Elsherbini <yahia.elsherbini@elements.nl>
2020-05-08 12:56:31 +0200

    Changed db name in an deploy script that was missed

--- deploy/import/import.sh
+++ deploy/import/import.sh
@@ -41,7 +41,7 @@ echo "Importing data"
 dc run --rm importer
 
 echo "Drop bag tables used for importing"
-docker-compose -p iiif-metadata-server -f ${DIR}/docker-compose.yml exec -T database psql -U stadsarchief -c 'DROP TABLE bag_openbareruimte, bag_verblijfsobjectpandrelatie, bag_pand, bag_nummeraanduiding, bag_standplaats, bag_ligplaats, bag_verblijfsobject' stadsarchief
+docker-compose -p iiif-metadata-server -f ${DIR}/docker-compose.yml exec -T database psql -U iiif_metadata_server -c 'DROP TABLE bag_openbareruimte, bag_verblijfsobjectpandrelatie, bag_pand, bag_nummeraanduiding, bag_standplaats, bag_ligplaats, bag_verblijfsobject' iiif_metadata_server
 
 echo "Running backups"
 dc exec -T database backup-db.sh iiif_metadata_server
