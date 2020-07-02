61b5957be3a6dda798e2b806b5757899ad58efe1
Dave <10104906+daveneijsen@users.noreply.github.com>
2020-07-02 15:36:43 +0200

    Update docker-migrate.sh

--- deploy/docker-migrate.sh
+++ deploy/docker-migrate.sh
@@ -6,7 +6,7 @@ set -x   # print what we are doing
 
 # Default credentials are for read-only access to the database.
 # Migration needs write access, so overwrite the default credentials.
-DATABASE_USER=${DATABASE_USER_WRITE}
-DATABASE_PASSWORD=${DATABASE_PASSWORD_WRITE}
+export DATABASE_USER=${DATABASE_USER_WRITE}
+export DATABASE_PASSWORD=${DATABASE_PASSWORD_WRITE}
 
 python manage.py migrate --noinput
