c27ef8d5289342f7c71a940e0be0ce02d844a7ec
Dave Neijsen <dave@focusautomation.nl>
2020-07-02 15:17:10 +0200

    Use explicit write permissions for migration

--- deploy/docker-migrate.sh
+++ deploy/docker-migrate.sh
@@ -4,4 +4,9 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x   # print what we are doing
 
+# Default credentials are for read-only access to the database.
+# Migration needs write access, so overwrite the default credentials.
+DATABASE_USER=${DATABASE_USER_WRITE}
+DATABASE_PASSWORD=${DATABASE_PASSWORD_WRITE}
+
 python manage.py migrate --noinput
