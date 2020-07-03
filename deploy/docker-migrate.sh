3abbfd81bea5666b658f4907123461238c0ea87b
Dave Neijsen <dave@focusautomation.nl>
2020-07-03 12:37:21 +0200

    Fix import script when not called from Openstack deployment

--- deploy/docker-migrate.sh
+++ deploy/docker-migrate.sh
@@ -4,9 +4,13 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x   # print what we are doing
 
-# Default credentials are for read-only access to the database.
-# Migration needs write access, so overwrite the default credentials.
-export DATABASE_HOST=${DATABASE_HOST_WRITE}
-export DATABASE_USER=${DATABASE_USER_WRITE}
-export DATABASE_PASSWORD=${DATABASE_PASSWORD_WRITE}
-python manage.py migrate --noinput
+# When executing from Openstack deployment, the default credentials are for 
+# read-only access to the database. Migration needs write access, 
+# so overwrite the default credentials.
+if [ ! -z ${DATABASE_HOST_WRITE+x} ]; then
+  export DATABASE_HOST=${DATABASE_HOST_WRITE}
+  export DATABASE_USER=${DATABASE_USER_WRITE}
+  export DATABASE_PASSWORD=${DATABASE_PASSWORD_WRITE}
+fi
+
+#python manage.py migrate --noinput
