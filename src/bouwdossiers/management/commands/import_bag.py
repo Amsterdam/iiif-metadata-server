b13dfff85979f1cabcaab9582df14091d6a359d3
Terry van Walen <t.van.walen@amsterdam.nl>
2024-02-05 14:05:10 +0100

    Move import_bag to bouwdossiers and update makefile entry

--- src/bouwdossiers/management/commands/import_bag.py
+++ src/bouwdossiers/management/commands/import_bag.py
@@ -6,26 +6,26 @@ from django.db import connection
 from bag.bag_loader import BagLoader
 from bag.koppeltabel_loader import KoppeltabelLoader
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 class BagRetrievalError(Exception):
     pass
 
-
+# TODO: Remove comments and split into functions to improve readability
 class Command(BaseCommand):
-    help = "Import BAG tables from datadienst export"
+    help = "Import BAG tables from datadienst export into new schema"
 
     def handle(self, *args, **options):
         bag = BagLoader()
         try:
             new_schema_name = "temp"
             
+            # Create fresh new schema
             with connection.cursor() as cursor:
                 cursor.execute(f"DROP SCHEMA IF EXISTS {new_schema_name} CASCADE;;")
                 cursor.execute(f"CREATE SCHEMA {new_schema_name};")
-            
-            self.stdout.write(self.style.SUCCESS(f'Schema "{new_schema_name}" created.'))
+            log.info(f'Schema "{new_schema_name}" created.')
 
             # Get a list of tables in the default schema
             tables = []
@@ -37,17 +37,18 @@ class Command(BaseCommand):
             with connection.cursor() as cursor:
                 for table in tables:
                    cursor.execute(f"CREATE TABLE {new_schema_name}.{table} (LIKE public.{table} INCLUDING ALL);")
+            log.info(f"Duplicated tables into schema: {new_schema_name}")
 
-
+            # Load bag into new schema
             with connection.schema_editor() as schema_editor:
                 schema_editor.execute(f'SET search_path TO {new_schema_name};')
                 bag.load_all_tables(new_schema_name)
                 schema_editor.execute(f'SET search_path TO public;')
-
-            # # The link between Pand and Verblijfsobject is not available through the CSV API and needs to be
-            # # constructed manually
-            # koppeltabel = KoppeltabelLoader()
-            # koppeltabel.load()
+            
+            # The link between Pand and Verblijfsobject is not available through the CSV API and needs to be
+            # constructed manually for now
+            koppeltabel = KoppeltabelLoader()
+            koppeltabel.load()
 
         except Exception as e:
             raise Exception(
