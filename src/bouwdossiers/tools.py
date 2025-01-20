aa6009b777ec1cf7de72ee4982b2424937a5e425
manilde van der Oord <m.van.der.oord@amsterdam.nl>
2025-01-20 11:11:51 +0100

    rewrite tools function

--- src/bouwdossiers/tools.py
+++ src/bouwdossiers/tools.py
@@ -13,16 +13,17 @@ def separate_dossier(dossier):
     If format is not correct, an APIException is raised
     """
 
-    pklist= dossier.upper().split('_')
+    m = re.match(r"([A-Za-z]{2,4})_([A-Za-z0-9-]+)", dossier)
 
     # Check if dossier has correct format       
-    if len(pklist) != 2 or not re.match(r"^\D{2,4}$", pklist[0]):
+    if not m:
         raise InvalidDossier(
             f"The dossier {dossier} is not of the correct form. "
             "It should be defined in the form of 'AA_P123456' in which "
             "AA (or AAA or AAAA; a max of four characters) is the stadsdeel code and P123456 is the dossier number"
         )
 
-    stadsdeel, dossiernr = pklist[0], pklist[1]
+    stadsdeel = m.group(1).upper()
+    dossiernr = m.group(2)
 
     return stadsdeel, dossiernr
