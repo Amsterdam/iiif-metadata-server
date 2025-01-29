90d91081bee32e3a4b5847b510f2d860dec61019
ferhany2002 <ferhan.y2002@gmail.com>
2025-01-29 11:27:20 +0100

    Update bag import code

--- src/bag/bag_controller.py
+++ src/bag/bag_controller.py
@@ -1,16 +1,35 @@
 import logging
+import os
 from datetime import datetime
 
+from django.conf import settings
+from django.db import connection
 from isodate import parse_date, parse_datetime
 from toolz import interleave, partial, pipe
 
-from bag.models import Nummeraanduiding, Pand
+from bag.models import (
+    Nummeraanduiding,
+    Pand,
+    Verblijfsobject,
+    Verblijfsobjectpandrelatie,
+)
 from bag.utils import chunk_data
 
 logger = logging.getLogger(__name__)
 
 
 class BagController:
+    def create_rijtjeshuizen_tables(self):
+        files = ["determine_building_units.sql", "cluster_building_dwelling.sql"]
+        folder = os.path.join(settings.BASE_DIR, "bag/raw_sql")
+        for file in files:
+            with open(os.path.join(folder, file), "r") as sql_file:
+                raw_sql = sql_file.read()
+
+                logger.info(f"Creating table from file {file}")
+                with connection.cursor() as cursor:
+                    cursor.execute(raw_sql)
+
     def create_bag_instances(self, bag_model, row: dict):
         bag_dict = {}
         for field, value in row.items():
@@ -49,7 +68,12 @@ class BagController:
                 isinstance(obj, Nummeraanduiding)
                 and obj.verblijfsobject_id == "0363010011290888"
             )
-            and not (isinstance(obj, Pand))
+            and not (
+                # https://api.data.amsterdam.nl/v1/bag/panden/?ligtInBouwblok.identificatie=03630012098117
+                # or https://api.data.amsterdam.nl/v1/gebieden/bouwblokken/?identificatie=03630012098117
+                isinstance(obj, Pand)
+                and obj.bouwblok_id == "03630012098117"
+            )
         )
 
     def upsert_table_data(self, bag_objects: iter):
@@ -87,6 +111,40 @@ class BagController:
             partial(map, self.upsert_table_data),
             interleave,
         )
+
+    @staticmethod
+    def filter_valid_references(bag_model, records, *, threshold=5):
+        if bag_model != Verblijfsobjectpandrelatie:
+            return records
+
+        # We need to read records more than once
+        records = list(records)
+
+        db_vots = set(Verblijfsobject.objects.values_list("id", flat=True))
+        db_panden = set(Pand.objects.values_list("id", flat=True))
+
+        valid_records = [
+            r
+            for r in records
+            if r["pand"] in db_panden and r["verblijfsobject"] in db_vots
+        ]
+        valid_record_vots = set(r["verblijfsobject"] for r in valid_records)
+        non_valid_vots = db_vots - valid_record_vots
+        num_db_vots_with_nonvalid_record = len(non_valid_vots)
+
+        if num_db_vots_with_nonvalid_record > threshold:
+            # Neem de eerste 10 niet-geldige verblijfsobject-ID's
+            example_ids = list(non_valid_vots)[:10]
+            example_text = ", ".join(map(str, example_ids))
+
+            raise Exception(
+                f"An exception occurred constructing records: {num_db_vots_with_nonvalid_record} "
+                f"{Verblijfsobject._meta.model_name}(en) in the database cannot be matched to a valid "
+                f"{Pand._meta.model_name} using the {Verblijfsobjectpandrelatie._meta.model_name} records. "
+                f"This exceeds the accepted threshold of {threshold}. Example IDs: {example_text}"
+            )
+
+        return valid_records
 
     @staticmethod
     def delete_nonmodified_table_records(bag_model, upserted_pk_indices: list[int]):
