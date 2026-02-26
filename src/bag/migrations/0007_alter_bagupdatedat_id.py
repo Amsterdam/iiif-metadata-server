c2e8f58a4fad6bcaba3b1f60dbc9740e0c201cfb
vanbuiten <david.vanbuiten@gmail.com>
2026-02-26 14:32:18 +0100

    fix: linting issues

--- src/bag/migrations/0007_alter_bagupdatedat_id.py
+++ src/bag/migrations/0007_alter_bagupdatedat_id.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('bag', '0006_alter_verblijfsobjectpandrelatie_pand_and_more'),
+        ("bag", "0006_alter_verblijfsobjectpandrelatie_pand_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='bagupdatedat',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="bagupdatedat",
+            name="id",
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID"),
         ),
     ]
